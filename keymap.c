04ef5426cc735152cc40cfdb8851b38818c01b2b
Ryan Miller <RyanMillerC@outlook.com>
2024-03-03 21:44:26 -0500

    I use Windows again...

--- keymap.c
+++ keymap.c
@@ -8,60 +8,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |--------------------------------------------------------------------------------------|
    * |  Tab  |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  \   |
    * |--------------------------------------------------------------------------------------|
-   * |   FN1  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |   Enter   |
+   * |   FN  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |   Enter   |
    * |--------------------------------------------------------------------------------------|
    * |    Shift   |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |   Shift     |
    * |--------------------------------------------------------------------------------------|
-   * |  Ctrl |  GUI  |  Alt  |            Space             |  Alt  |  GUI  |  FN2  | Ctrl  |
+   * |  Ctrl |  Alt  |  Ctrl |            Space             |  Ctrl |  Alt  |  GUI  |  FN   |
    * `--------------------------------------------------------------------------------------'
    */
   LAYOUT(
-    KC_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,
+    QK_GESC, KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,    KC_8,  KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,  KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
     MO(1),   KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,  KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT, KC_NO,   KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_NO,
-    KC_LCTL, KC_LGUI, KC_LALT,                 KC_SPC, KC_SPC, KC_SPC,         KC_RALT, KC_RGUI, KC_NO,   MO(2),   KC_RCTL
+    KC_LCTL, KC_LALT, KC_LCTL,                 KC_SPC, KC_SPC, KC_SPC,         KC_RCTL, KC_RALT, KC_NO,   KC_RGUI, MO(1)
   ),
 
   /* Layer 1
    * ,--------------------------------------------------------------------------------------.
-   * |  ~  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |  Del   |
+   * |  ~  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | Vol-| Vol+|  Del   |
    * |--------------------------------------------------------------------------------------|
-   * | CAPS  |ALTF4|     |     |     |     |     |     |     |     |     |P.DWN|P. UP|      |
+   * | CAPS  |ALTF4|     |     |     |     |     |     |     |     |PrntS|P.DWN|P. UP|      |
    * |--------------------------------------------------------------------------------------|
    * |        |     |     |     |     |     | Left| Down| Up  |Right|     |     |           |
    * |--------------------------------------------------------------------------------------|
-   * |            |     |     |     |     |     |     |     |     |     |     |             |
+   * |            |     |     |     |     |     |     |     | Bri-| Bri+|     |             |
    * |--------------------------------------------------------------------------------------|
    * |       |       |       |                              |       |       |       |       |
    * `--------------------------------------------------------------------------------------'
    */
   LAYOUT(
-    KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_DEL,
+    KC_GRV,  KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_TRNS, KC_DEL,
     KC_CAPS, A(KC_F4), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
     KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,          KC_TRNS,
-    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-
-  /* Layer 2
-   * ,--------------------------------------------------------------------------------------.
-   * |RESET|     |     |     |     |     |     |     |     |     |     |     |     |        |
-   * |--------------------------------------------------------------------------------------|
-   * |      |RGB T| RGB | RGB | RGB | RGB | RGB | RGB | RGB |     |     |     |     |       |
-   * |--------------------------------------------------------------------------------------|
-   * |        |     |     |     |     |     |     |     |     |     |     |     |           |
-   * |--------------------------------------------------------------------------------------|
-   * |            |     |     |     |     |     |     |     |     |     |     |             |
-   * |--------------------------------------------------------------------------------------|
-   * |       |       |       |                              |       |       |       |       |
-   * `--------------------------------------------------------------------------------------'
-   */
-  LAYOUT(
-    RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,  KC_TRNS,                  KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_LGUI, KC_LGUI, KC_LGUI,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
---
+++ rules.mk
@@ -0,0 +1,5 @@
+# I'm not sure which of these is actually disabling the DZ60 lights
+BACKLIGHT_ENABLE = no
+LED_MATRIX_ENABLE = no
+LED_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = no
